f181d419c47f74c287c3fee82ced2e4f635ebd11
Aditya Rawat <adityarawat240@gmail.com>
2025-04-08 02:05:30 +0530

    clerk-frontend-base

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
@@ -19,8 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en" className="dark">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+            {/* <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut> 
+            <SignedIn>
+              <UserButton />
+            </SignedIn> */}
+          </header>
+        {children}
+      </body>
     </html>
+    </ClerkProvider>
   );
 }
