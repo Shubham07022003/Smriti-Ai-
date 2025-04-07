814154e770c988741ab6e0e7f4f9f0a5136fe33d
Vatsal Bhakodia <123301652+vatsal-bhakodia@users.noreply.github.com>
2025-04-07 21:20:56 +0530

    hero, navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,103 +1,87 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { motion } from "framer-motion";
+import React from "react";
+import BlurIn from "@/components/magicui/blur-in";
+import AnimatedImage from "@/components/AnimatedImage";
+import Link from "next/link";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import { NumberTicker } from "@/components/magicui/number-ticker";
+import { Brain } from "lucide-react";
+
+import Navbar from "@/components/navbar";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+export default function Hero() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div>
+      <Navbar />
+      {/* Hero Section */}
+      <div className="z-0 relative min-h-screen w-full overflow-hidden">
+        <motion.div
+          className="relative z-10 flex flex-col items-center justify-start min-h-screen space-y-4 px-4 pt-32 pb-12"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <DotPattern
+            className={cn(
+              "absolute inset-0 z-0 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)] dark:[mask-image:radial-gradient(50vw_circle_at_center,black,transparent)]"
+            )}
+          />
+          <p className="flex items-center mb-6 border border-neutral-300 rounded-3xl text-xs px-3 py-[3px] shadow-[3px_3px_0px_0px_rgb(143,143,143)]">
+            <Brain className="me-[5px] h-3 w-3" />
+            Remember Smarter
+          </p>
+          <motion.div variants={itemVariants}>
+            <BlurIn
+              word={
+                <>
+                  <span>Watch. Read. </span>
+                  <br />
+                  <SparklesText className="inline" text="Remember." />
+                  <span> Repeat</span>
+                </>
+              }
+              className="font-display text-center text-4xl font-bold w-full lg:w-auto max-w-4xl mx-auto -z-10"
+              duration={1}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          </motion.div>
+          <motion.h2
+            className="text-xl text-muted-foreground tracking-normal text-center max-w-2xl mx-auto z-10"
+            variants={itemVariants}
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Upload course videos and PDFs, we'll create personalized MCQs and
+            send them to you on WhatsApp to help you{" "}
+            <NumberTicker value={100} />% retain what you learn.
+          </motion.h2>
+          <motion.div variants={itemVariants} className="z-20">
+            <Link href="/dashboard">
+              <button className="btn-primary">Get Started</button>
+            </Link>
+          </motion.div>
+          <motion.div variants={itemVariants}>
+            <AnimatedImage
+              src="/dummy.svg"
+              alt="Image"
+              width={1200}
+              height={900}
+              className="w-full h-auto max-w-6xl mx-auto rounded-2xl shadow-lg px-0 sm:px-4"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 }
