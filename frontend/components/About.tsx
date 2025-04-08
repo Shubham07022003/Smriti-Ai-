9fc2d7750e1c9aa33292114385cbe9e959e0ead1
utsavg05 <utsav.gp1204@gmail.com>
2025-04-08 23:09:06 +0530

    about pricing added

---
+++ frontend/components/About.tsx
@@ -0,0 +1,13 @@
+import AboutIntro from "./AboutIntro"
+import FeaturesGrid from "./FeaturesGrid"
+
+const About = () => {
+  return (
+    <>
+      <AboutIntro />
+      <FeaturesGrid />
+    </>
+  )
+}
+
+export default About;
