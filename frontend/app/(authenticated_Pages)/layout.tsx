2806b640cf838373d480ce72fca50b9282de3c4d
Vatsal Bhakodia <123301652+vatsal-bhakodia@users.noreply.github.com>
2025-04-11 01:04:29 +0530

    Refactor AuthGate component and update backend URI

--- frontend/app/(authenticated_Pages)/layout.tsx
+++ frontend/app/(authenticated_Pages)/layout.tsx
@@ -1,31 +1,9 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "../../app/globals.css";
 import AuthGate from "@/components/AuthGate";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["400", "500", "600", "700"],
-});
-
-export const metadata: Metadata = {
-  title: "Smriti AI",
-  description: "Smriti AI - Remember Smarter",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-<<<<<<< HEAD
-      // <AuthGate>{children}</AuthGate>
-      <div>{children}</div>
-=======
-    <AuthGate>{children}</AuthGate>
-    // <div>{children}</div>
->>>>>>> 8b6df53653452efae072141f8af0ba5f9d4ac6e5
-  );
+  return <AuthGate>{children}</AuthGate>;
 }
